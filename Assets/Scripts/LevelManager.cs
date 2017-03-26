da8bdde21f3edde135e3d5125f88c99d49052b04
AlexanderStepchkov <stepchkov@gmail.com>
2017-03-26 19:08:17 +0300

    133

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Debug.Log("App is closing");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip startMucic;
+    [SerializeField]
+    private LevelManager loadManager;
+
+	void Awake () {
+        GameObject.DontDestroyOnLoad(gameObject);
+        audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+    void Start() {
+        audioSource.clip = startMucic;
+        audioSource.Play();
+        Invoke("LoadNextLevel", startMucic.length);
+    }
+
+    void LoadNextLevel() {
+        loadManager.LoadLevel("01 Start");
+    }
+}
